3a31673f32e1ab0f969beb8df0c129ffb8a4ad53
nekosune <kat.swales@nekokittygames.com>
2013-09-17 16:23:30 +0100

    Added chapter nav, simple for now

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,3 +1,18 @@
 class Chapter < ActiveRecord::Base
-	belongs_to :Story
+	belongs_to :story
+
+	def prev?
+		return number >1
+	end
+
+	def next?
+		return number < story.chapters.count
+	end
+	def prev
+		return Chapter.find_by_number_and_story_id(number-1,story_id)
+	end
+	def next
+		return Chapter.find_by_number_and_story_id(number+1,story_id)
+	end
+
 end
